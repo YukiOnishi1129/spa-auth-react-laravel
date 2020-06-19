f975b556287f591f1689bf2237f54af051b8f8d0
大西雄貴 <yuki.onishi1129@gmail.com>
2020-06-19 23:58:10 +0900

    CORS setup

--- laravel-docker-workspace/laravel-auth/config/cors.php
+++ laravel-docker-workspace/laravel-auth/config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'], // どのエンドポイントを保護するかを示す
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['http://localhost:3000'], //リクエストが受け入れられるURLを指定する
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +29,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
